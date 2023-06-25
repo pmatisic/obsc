48c2187283edbf25e98182c345277ee57e8078a2
Petar <matisicpetar@gmail.com>
2023-06-26 01:58:14 +0200

    updated parameters

--- rjesenje.py
+++ rjesenje.py
@@ -26,14 +26,30 @@ class Jedinica:
             ukupnaPovrsina += baterija.duzina * baterija.sirina
             if (baterija.x + baterija.duzina > spremnik.sirina) or (baterija.y + baterija.sirina > spremnik.sirina):
                 return 0
-            for j, drugaBaterija in enumerate(self.baterije):
-                if i != j:
-                    if (baterija.x < drugaBaterija.x + drugaBaterija.duzina and
-                            baterija.x + baterija.duzina > drugaBaterija.x and
-                            baterija.y < drugaBaterija.y + drugaBaterija.sirina and
-                            baterija.y + baterija.sirina > drugaBaterija.y):
-                        return 0
+            for j, drugaBaterija in enumerate(self.baterije[i + 1:]):
+                if self.preklapajuSe(baterija, drugaBaterija):
+                    return 0
         return ukupnaPovrsina
+
+    @staticmethod
+    def preklapajuSe(b1, b2):
+        tockeNaB1 = [(np.cos(t) * b1.duzina, np.sin(t) * b1.sirina) for t in np.linspace(0, 2 * np.pi, 100)]
+        rotiraneTockeB1 = [Jedinica.rotirajTocku(tocka, np.radians(b1.rotacija)) for tocka in tockeNaB1]
+        rotiraneTockeB1 = [(x + b1.x, y + b1.y) for x, y in rotiraneTockeB1]
+        
+        tockeNaB2 = [(np.cos(t) * b2.duzina, np.sin(t) * b2.sirina) for t in np.linspace(0, 2 * np.pi, 100)]
+        rotiraneTockeB2 = [Jedinica.rotirajTocku(tocka, np.radians(b2.rotacija)) for tocka in tockeNaB2]
+        rotiraneTockeB2 = [(x + b2.x, y + b2.y) for x, y in rotiraneTockeB2]
+        
+        osi = Jedinica.normalniVektoriElipse(b1.duzina, b1.sirina, np.radians(b1.rotacija)) + \
+              Jedinica.normalniVektoriElipse(b2.duzina, b2.sirina, np.radians(b2.rotacija))
+
+        for os in osi:
+            proj1 = Jedinica.projekcijaTocakaNaOs(rotiraneTockeB1, os)
+            proj2 = Jedinica.projekcijaTocakaNaOs(rotiraneTockeB2, os)
+            if not Jedinica.intervalPreklapanja(proj1, proj2):
+                return False
+        return True
 
     def provjeriPreklapanje(self, indeks):
         b1 = self.baterije[indeks]
@@ -74,30 +90,29 @@ class Jedinica:
         tockeNaElipsi = [(np.cos(t) * baterija.duzina, np.sin(t) * baterija.sirina) for t in np.linspace(0, 2 * np.pi, 100)]
 
         while True:
-            baterija.x = random.randint(0, 1000 * (spremnik.sirina - baterija.duzina)) / 1000.0
-            baterija.y = random.randint(0, 1000 * (spremnik.sirina - baterija.sirina)) / 1000.0
-            baterija.rotacija = random.randint(0, 360000) / 1000.0
+            baterija.x = random.uniform(0, spremnik.sirina - baterija.duzina)
+            baterija.y = random.uniform(0, spremnik.sirina - baterija.sirina)
+            baterija.rotacija = random.randint(0, 179999) / 1000.0
             rotiraneTocke = [Jedinica.rotirajTocku(tocka, np.radians(baterija.rotacija)) for tocka in tockeNaElipsi]
+            rotiraneTocke = [(x + baterija.x, y + baterija.y) for x, y in rotiraneTocke]
             preklapanje = False
 
             for drugaBaterija in self.baterije:
                 if drugaBaterija != baterija:
                     tockeNaDrugojElipsi = [(np.cos(t) * drugaBaterija.duzina, np.sin(t) * drugaBaterija.sirina) for t in np.linspace(0, 2 * np.pi, 100)]
                     rotiraneTockeDrugeBaterije = [Jedinica.rotirajTocku(tocka, np.radians(drugaBaterija.rotacija)) for tocka in tockeNaDrugojElipsi]
+                    rotiraneTockeDrugeBaterije = [(x + drugaBaterija.x, y + drugaBaterija.y) for x, y in rotiraneTockeDrugeBaterije]
                     osi = Jedinica.normalniVektoriElipse(baterija.duzina, baterija.sirina, np.radians(baterija.rotacija)) + \
                           Jedinica.normalniVektoriElipse(drugaBaterija.duzina, drugaBaterija.sirina, np.radians(drugaBaterija.rotacija))
-                    for os in osi:
-                        projekcijaPrve = Jedinica.projekcijaTocakaNaOs(rotiraneTocke, os)
-                        projekcijaDruge = Jedinica.projekcijaTocakaNaOs(rotiraneTockeDrugeBaterije, os)
-                        if not Jedinica.intervalPreklapanja(projekcijaPrve, projekcijaDruge):
-                            preklapanje = False
-                            break
+                    if all(Jedinica.intervalPreklapanja(Jedinica.projekcijaTocakaNaOs(rotiraneTocke, os), 
+                                                        Jedinica.projekcijaTocakaNaOs(rotiraneTockeDrugeBaterije, os)) for os in osi):
                         preklapanje = True
+                        break
+
+            brojPokusaja += 1
 
             if not preklapanje or brojPokusaja > 100:
                 break
-            
-            brojPokusaja += 1
 
     @staticmethod
     def krizaj(roditelj1, roditelj2):
@@ -126,8 +141,8 @@ class GenetskiAlgoritam:
             for b in self.baterije:
                 maksX = self.spremnik.sirina - b.duzina
                 maksY = self.spremnik.sirina - b.sirina
-                x = random.uniform(0, maksX / self.spremnik.sirina)
-                y = random.uniform(0, maksY / self.spremnik.sirina)
+                x = random.uniform(0, maksX)
+                y = random.uniform(0, maksY)
                 jedinicaBaterije.append(Baterija(b.duzina, b.sirina, x, y, b.rotacija))
             populacija.append(Jedinica(jedinicaBaterije))
         return populacija
@@ -135,10 +150,11 @@ class GenetskiAlgoritam:
     def pokreni(self):
         for generacija in range(self.brojGeneracija):
             dobrote = [jedinica.izracunajFitness(self.spremnik) for jedinica in self.populacija]
-
             if sum(dobrote) == 0:
-                roditelj1 = random.choice(self.populacija)
-                roditelj2 = random.choice(self.populacija)
+                indeksi = range(len(self.populacija))
+                izabraniIndeksi = random.sample(indeksi, 2)
+                roditelj1 = self.populacija[izabraniIndeksi[0]]
+                roditelj2 = self.populacija[izabraniIndeksi[1]]
             else:
                 roditelj1 = random.choices(self.populacija, weights=dobrote)[0]
                 roditelj2 = random.choices(self.populacija, weights=dobrote)[0]
@@ -167,33 +183,34 @@ def vizualizirajRjesenje(spremnik, jedinica):
     ax.set_xlim(0, spremnik.sirina)
     ax.set_ylim(0, spremnik.sirina)
     ax.set_aspect('equal', adjustable='box')
-
     for baterija in jedinica.baterije:
-        elipsa = patches.Ellipse((baterija.x, baterija.y), baterija.duzina, baterija.sirina, angle=baterija.rotacija,
-                                 linewidth=1, edgecolor='r', facecolor='none')
+        centarX = baterija.x + baterija.duzina / 2.0
+        centarY = baterija.y + baterija.sirina / 2.0
+        elipsa = patches.Ellipse((centarX, centarY), baterija.duzina, baterija.sirina, angle=baterija.rotacija, linewidth=1, edgecolor='r', facecolor='none')
         ax.add_patch(elipsa)
-
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Vizualizacija rješenja')
-    plt.grid(True)
+    plt.xlim(0, spremnik.sirina)
+    plt.ylim(0, spremnik.sirina)
+    plt.gca().set_aspect('equal', adjustable='box')
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("Vizualizacija rasporeda baterija")
     plt.show()
 
-spremnik = Spremnik(1_000_000)
-baterije = [Baterija(40_000, 20_000, 0, 0, random.randint(0, 1_000_000)) for _ in range(39)] + \
-           [Baterija(20_000, 10_000, 0, 0, random.randint(0, 1_000_000)) for _ in range(161)]
-velicinaPopulacije = 100
-brojGeneracija = 1000
-stopaMutacije = 0.1
-stopaKrizanja = 0.9
+if __name__ == "__main__":
+    spremnik = Spremnik(1_000_000)
+    baterije = [Baterija(40_000, 20_000, 0, 0, random.randint(0, 1_000_000)) for _ in range(39)] + \
+               [Baterija(20_000, 10_000, 0, 0, random.randint(0, 1_000_000)) for _ in range(161)]
+    velicinaPopulacije = 100
+    brojGeneracija = 1000
+    stopaMutacije = 0.1
+    stopaKrizanja = 0.8
+    genetskiAlgoritam = GenetskiAlgoritam(velicinaPopulacije, brojGeneracija, stopaMutacije, stopaKrizanja, spremnik, baterije)
+    najboljeRjesenje = genetskiAlgoritam.pokreni()
 
-genetskiAlgoritam = GenetskiAlgoritam(velicinaPopulacije, brojGeneracija, stopaMutacije, stopaKrizanja, spremnik, baterije)
-najboljeRjesenje = genetskiAlgoritam.pokreni()
+    print("Najbolje rješenje:", najboljeRjesenje)
+    with open('rjesenje.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        for baterija in najboljeRjesenje.baterije:
+            writer.writerow([int(baterija.x), int(baterija.y), baterija.duzina, baterija.sirina, int(baterija.rotacija)])
 
-print("Najbolje rješenje:", najboljeRjesenje)
-with open('rjesenje.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    for baterija in najboljeRjesenje.baterije:
-        writer.writerow([int(baterija.x*1000), int(baterija.y*1000), baterija.duzina, baterija.sirina, int(baterija.rotacija*1000)])
-
-vizualizirajRjesenje(spremnik, najboljeRjesenje)
+    vizualizirajRjesenje(spremnik, najboljeRjesenje)
